fd2f0379b3efd90852758917cd24999121b3fd07
Nolan Byrne <nolan.byrne@comcast.net>
2020-09-21 19:37:13 -0500

    Cleaned up deckretriever

--- Deckorator/Services/DeckRetriever.cs
+++ Deckorator/Services/DeckRetriever.cs
@@ -1,9 +1,4 @@
-﻿using MtgApiManager.Lib.Core;
-using MtgApiManager.Lib.Model;
-using MtgApiManager.Lib.Service;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -12,34 +7,12 @@ namespace Deckorator.Services
     public class DeckRetriever
     {
         private readonly HttpClient httpClient;
-        private readonly CardService mtgApi;
 
-        public DeckRetriever(HttpClient httpClient, CardService mtgApi)
+        public DeckRetriever(HttpClient httpClient)
         {
             this.httpClient = httpClient;
-            this.mtgApi = mtgApi;
         }
 
-        //public string GetRandomDeckUrl()
-        //{
-        //    Exceptional<List<Card>> creaturesRequest = mtgApi.Where(x => x.GameFormat, "Commander")
-        //                     .Where(x => x.SuperTypes, new string[] { "Legendary", "Creature" })
-        //                     .All();
-
-        //    Exceptional<List<Card>> planeswalkersRequest = mtgApi.Where(x => x.GameFormat, "Commander").Where(x => x.SuperTypes, new string[] { "Planeswalker" }).Where(x => x.Text, "can be your commander").All();
-  
-
-        //    if (creaturesRequest.IsSuccess && planeswalkersRequest.IsSuccess)
-        //    {
-        //        creaturesRequest.Value.AddRange(planeswalkersRequest.Value);
-        //        List<Card> commanders = new List<Card>(creaturesRequest.Value);
-        //        Random rng = new Random();
-
-        //        return JsonConvert.SerializeObject(commanders[rng.Next(commanders.Count)]);
-        //    }
-        //    else throw new Exception();
-        //}
-        
         public async Task<string> GetRandomDeckUrl()
         {
             var response = await httpClient.GetAsync("https://api.scryfall.com/cards/random?q=is%3Acommander");
