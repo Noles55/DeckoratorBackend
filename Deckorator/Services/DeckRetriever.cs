0552bacafb9d861f98676f2f56f515decfa3b644
Nolan Byrne <nolan.byrne@comcast.net>
2020-09-23 19:49:30 -0500

    Progress on getting random deck url from tappedout

--- Deckorator/Services/DeckRetriever.cs
+++ Deckorator/Services/DeckRetriever.cs
@@ -1,14 +1,18 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace Deckorator.Services
 {
     public class DeckRetriever
     {
         private readonly HttpClient httpClient;
+        private readonly string deckSourceQuery = "https://tappedout.net/mtg-decks/search/?general={0}&page={1}";
+        private readonly int startingPage = 20;
 
         public DeckRetriever(HttpClient httpClient)
         {
@@ -17,11 +21,30 @@ namespace Deckorator.Services
 
         public async Task<string> GetRandomDeckUrl()
         {
-            HttpResponseMessage response = await httpClient.GetAsync("https://api.scryfall.com/cards/random?q=is%3Acommander");
-            string json = await response.Content.ReadAsStringAsync();
-            JObject cardDict = JsonConvert.DeserializeObject<JObject>(json);
+            HttpResponseMessage commanderResponse = await httpClient.GetAsync("https://api.scryfall.com/cards/random?q=is%3Acommander");
+            string json = await commanderResponse.Content.ReadAsStringAsync();
+            string commander = JsonConvert.DeserializeObject<JObject>(json).GetValue("name").ToString().ToLower().Replace("\"", String.Empty).Replace(",", String.Empty).Replace(" ", "-");
+            Random random = new Random();
 
-            return cardDict.GetValue("name").ToString();
+            HttpResponseMessage decksResponse;
+            int currMaxPage = startingPage;
+            do
+            {
+                string decksQuery = string.Format(deckSourceQuery, new string[] { commander, (random.Next(currMaxPage) + 1).ToString() });
+                currMaxPage /= 2;
+                Console.WriteLine("TappedOut query: " + decksQuery);
+                decksResponse = await httpClient.GetAsync(decksQuery);
+            } while (!decksResponse.IsSuccessStatusCode && currMaxPage > 0);
+           
+            string htmlString = await decksResponse.Content.ReadAsStringAsync();
+
+            HtmlDocument html = new HtmlDocument();
+            html.LoadHtml(htmlString);
+
+            HtmlNodeCollection nodes = html.DocumentNode.SelectNodes("//h3[@class='name deck-wide-header']/a");
+
+            return "";
         }
     }
 }
+//*[@id="body"]/div[7]/div/div[1]/div[9]/div/div[2]/h3/a
